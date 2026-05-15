329d5bcde3b727d76dd48a18f7aa6672d698558f
Perplexboy <2839897253@qq.com>
2026-05-16 02:11:09 +0800

    增加了歌词

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -54,7 +54,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "玉置浩二",
             url: "/assets/music/Friend-玉置浩二.flac",
             cover: "/assets/music/cover/玉置浩二-Friend.png",
-            lrc: "",
+            lrc: "/assets/music/lrc/Friend-玉置浩二.lrc",
         },
         // 添加新歌：
         {
@@ -62,7 +62,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "James Blunt",
             url: "/assets/music/1973-James Blunt.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/James Blunt-1973.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/1973-James Blunt.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
         {
@@ -70,7 +70,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "ONE OK ROCK",
             url: "/assets/music/Stand Out Fit In-ONE OK ROCK.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/ONE OK ROCK-Stand Out Fit In.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Stand Out Fit In-ONE OK ROCK.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
         {
@@ -78,7 +78,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "矢野立美",
             url: "/assets/music/Brave Love, TIGA (PIANO VERSION)-矢野立美.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/矢野立美-Brave Love, TIGA (PIANO VERSION).png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Brave Love, TIGA (PIANO VERSION)-矢野立美.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
         {
@@ -86,7 +86,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Eagles",
             url: "/assets/music/Hotel California-Eagles.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Eagles-Hotel California.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Hotel California-Eagles.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
         {
@@ -94,7 +94,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Bob Marley",
             url: "/assets/music/No Woman, No Cry-Bob Marley.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Bob Marley-No Woman, No Cry.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/No Woman, No Cry-Bob Marley.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -102,15 +102,15 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Adele",
             url: "/assets/music/Set Fire to the Rain-Adele.m4a",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Adele-Set Fire to the Rain.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Set Fire to the Rain-Adele.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
             name: "Should It Matter",
             artist: "Sissel",
-            url: "/assets/music/Should It Matter-Sissel.flac",       // 放 public/assets/music/ 下
+            url: "/assets/music/Should It Matter-Sissel.mp3",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Sissel-Should It Matter.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Should It Matter-Sissel.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -118,7 +118,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Buckethead",
             url: "/assets/music/The Left Panel-Buckethead.mp3",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Buckethead-The Left Panel.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/The Left Panel-Buckethead.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -126,7 +126,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Eric Clapton",
             url: "/assets/music/Wonderful Tonight-Eric Clapton.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Eric Clapton-Wonderful Tonight.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Wonderful Tonight-Eric Clapton.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -134,15 +134,15 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "齐秦",
             url: "/assets/music/不让我的眼泪陪我过夜-齐秦.wav",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/齐秦-不让我的眼泪陪我过夜.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/不让我的眼泪陪我过夜-齐秦.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
             name: "黃昏",
             artist: "羅文",
-            url: "/assets/music/黃昏-羅文.wav",       // 放 public/assets/music/ 下
+            url: "/assets/music/黃昏-羅文.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/罗文-黄昏.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/黃昏-羅文.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -150,7 +150,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "陈慧琳",
             url: "/assets/music/记事本-陈慧琳.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/陈慧琳-记事本.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/记事本-陈慧琳.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -158,7 +158,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "HAYA",
             url: "/assets/music/寂靜的天空-HAYA.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/HAYA-寂靜的天空.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/寂靜的天空-HAYA.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -166,7 +166,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "李佳薇",
             url: "/assets/music/煎熬-李佳薇.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/李佳薇-煎熬.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/煎熬-李佳薇.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -174,7 +174,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "陈淑桦",
             url: "/assets/music/梦醒时分-陈淑桦.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/陈淑桦-梦醒时分.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/梦醒时分-陈淑桦.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -182,7 +182,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "张震岳 & 大渊(顽童MJ116)",
             url: "/assets/music/迷途羔羊-张震岳 & 大渊(顽童MJ116).flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/张震岳-迷途羔羊.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/迷途羔羊-张震岳 & 大渊(顽童MJ116).lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -190,7 +190,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "梁博",
             url: "/assets/music/日落大道-梁博.wav",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/梁博-日落大道.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/日落大道-梁博.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -198,7 +198,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "毛宁",
             url: "/assets/music/涛声依旧-毛宁.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/毛宁-涛声依旧.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/涛声依旧-毛宁.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -206,7 +206,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "梅沢富美男",
             url: "/assets/music/心ゆくまで-梅沢富美男.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/梅沢富美男-心ゆくまで.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/心ゆくまで-梅沢富美男.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
 		// 添加新歌：
 		{
@@ -214,7 +214,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "姬神",
             url: "/assets/music/一人静-姬神.wav",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/姬神-一人静.png",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/一人静-姬神.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
         // 添加新歌：
 		{
@@ -222,7 +222,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "童安格",
             url: "/assets/music/明天你是否依然爱我-童安格.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/童安格-明天你是否依然爱我.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/明天你是否依然爱我-童安格.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
         // 添加新歌：
 		{
@@ -230,7 +230,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "王杰",
             url: "/assets/music/一场游戏一场梦-王杰.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/王杰-一场游戏一场梦.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/一场游戏一场梦-王杰.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
         // 添加新歌：
 		{
@@ -238,7 +238,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "Lil Peep",
             url: "/assets/music/Haunt U (Prod.Mysticphonk)-Lil Peep.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/Lil Peep-Haunt U (Prod.Mysticphonk).jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/Haunt U (Prod.Mysticphonk)-Lil Peep.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
         // 添加新歌：
 		{
@@ -246,7 +246,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
             artist: "变奏的梦想",
             url: "/assets/music/江上清风游-变奏的梦想.flac",       // 放 public/assets/music/ 下
             cover: "/assets/music/cover/变奏的梦想-江上清风游.jpg",  // 放 public/assets/music/cover/ 下
-            lrc: "",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
+            lrc: "/assets/music/lrc/江上清风游-变奏的梦想.lrc",  // 可选：歌词文件路径如 "/assets/music/lrc/歌词.lrc"，或直接写 LRC 字符串
         },
     ],
 },
